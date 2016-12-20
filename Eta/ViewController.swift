074d210a1058d1a04b0274106240653bf2fd5d45
Indiana Jong <bertdejong91@gmail.com>
2016-12-20 17:32:02 +0100

    First commit

--- Eta/ViewController.swift
+++ Eta/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hey, This is my first commit Dome!!!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
